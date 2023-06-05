83be68025ec6e2a9b5fd8e82284a0eeebf6bb939
brianjien <brianjienop49792399@gmail.com>
2023-06-05 15:30:00 +0800

    Initial Commit

---
+++ Smart_Chat/Smart_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smart_ChatApp.swift
+//  Smart_Chat
+//
+//  Created by brianjien on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Smart_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
